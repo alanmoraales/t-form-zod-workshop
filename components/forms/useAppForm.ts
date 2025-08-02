db75e14c8d7346c6f0d0de4bd9c88074bfaf2a01
alanmoraales <alanmoraales845@gmail.com>
2025-08-02 13:03:02 -0600

    remove comments

--- components/forms/useAppForm.ts
+++ components/forms/useAppForm.ts
@@ -2,14 +2,12 @@ import { createFormHookContexts, createFormHook } from "@tanstack/react-form";
 import TextInput from "./TextInput";
 import SubmitButton from "./SubmitButton";
 
-// export useFieldContext for use in your custom components
 export const { fieldContext, formContext, useFieldContext, useFormContext } =
   createFormHookContexts();
 
 const { useAppForm, withForm } = createFormHook({
   fieldContext,
   formContext,
-  // We'll learn more about these options later
   fieldComponents: {
     TextInput,
   },
