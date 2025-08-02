bcae1b1d743378237b464b3172b8652c3766d375
alanmoraales <alanmoraales845@gmail.com>
2025-08-01 20:02:27 -0600

    fix theme

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import { RegisterForm } from "@/components/RegisterForm";
 
 export default function Home() {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-50">
+    <div className="min-h-screen flex items-center justify-center">
       <RegisterForm />
     </div>
   );
